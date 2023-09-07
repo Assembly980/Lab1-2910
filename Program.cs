00aff0cf734eeb97dcc18b6aa7c37f5e5bee4369
Assembly980 <113926702+Assembly980@users.noreply.github.com>
2023-09-07 10:16:45 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Twitty_Brandon_Lab1_2910
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+            string file = folder + Path.DirectorySeparatorChar + "videogames.csv";
+
+            List<VideoGame> games = new List<VideoGame>();
+
+            using(var reader = new StreamReader(file))
+            {
+                while(!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    string[] values = line.Split(',');
+
+                    VideoGame x = new VideoGame()
+                    {
+                        Name = values[0],
+                        Platform = values[1],
+                        Year = values[2], 
+                        Genre = values[3],
+                        Publisher = values[4],
+                        NA_Sales = values[5],
+                        EU_Sales = values[6],
+                        JP_Sales = values[7],
+                        Other_Sales = values[8],
+                        Global_Sales = values[9]
+                    };
+
+                    games.Add(x);
+                }   //End of While Loop
+            }  //End of Using statement 
+
+            games.Sort();
+            double numGames = games.Count();
+
+            Console.WriteLine("What Publisher do you want to look at?");
+            string userPublisher = Console.ReadLine();
+            PublisherData(games, userPublisher);
+            
+
+            Console.WriteLine("\n\n\n\n\n\n");
+
+            Console.WriteLine("What Genre do you want to look at?");
+            string userGenre = Console.ReadLine();
+            GenreData(games, userGenre);
+            
+            
+        }
+
+        static void PublisherData(List<VideoGame> games, string publisher)
+        {
+            var publisherGames = games.Where(x => x.Publisher == publisher);
+            foreach (var pub in publisherGames) Console.WriteLine(pub.ToString());
+
+            double numGames = games.Count();
+
+            var ubisoftGames = games.Where(x => x.Publisher == publisher);
+            double numPublisher = ubisoftGames.Count();
+            double percentPublisher = Math.Round((numPublisher / numGames * 100), 2);
+            Console.WriteLine($"Of the {numGames} games, {numPublisher} are by Ubisoft games, which is {percentPublisher}%");
+        }
+
+        static void GenreData(List<VideoGame> games, string genre)
+        {
+            var genreGames = games.Where(x => x.Genre == genre);
+            foreach (var gen in genreGames) Console.WriteLine(gen.ToString());
+
+            double numGames = games.Count();
+
+            var Creeper_____Aww_Man = games.Where(X => X.Genre == genre);
+            double NumGen = Creeper_____Aww_Man.Count();
+            double percentGen = Math.Round((NumGen * 100 / numGames), 2);
+            Console.WriteLine($"Of the {numGames} games, {NumGen} are Simulation games, which is {percentGen}%");
+        }
+    }
+}
---
+++ VideoGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Twitty_Brandon_Lab1_2910
+{
+    public class VideoGame : IComparable<VideoGame>
+    {
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public string Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public string NA_Sales { get; set; }
+        public string EU_Sales { get; set; }
+        public string JP_Sales { get; set; }
+        public string Other_Sales { get; set; }
+        public string Global_Sales { get; set; }
+
+        public VideoGame() { }
+        
+        public VideoGame(string name, string platform, string year, string genre, string publisher, string na_sales, string eu_sales, string jp_sales, string other_sales, string global_sales)
+        {
+            this.Name = name;
+            this.Platform = platform;
+            this.Year = year;
+            this.Genre = genre;
+            this.Publisher = publisher;
+            this.NA_Sales = na_sales;
+            this.EU_Sales = eu_sales;
+            this.JP_Sales = jp_sales;
+            this.Other_Sales = other_sales;
+            this.Global_Sales = global_sales;
+        }
+
+        public int CompareTo(VideoGame other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            string msg = $"Name: {this.Name}\n";
+            msg += $"Platform: {this.Platform}\n";
+            msg += $"Year: {this.Year}\n";
+            msg += $"Genre: {this.Genre}\n";
+            msg += $"Publisher: {this.Publisher}\n";
+            msg += $"NA Sales: {this.NA_Sales}\n";
+            msg += $"EU Sales: {this.EU_Sales}\n";
+            msg += $"JP Sales: {this.JP_Sales}\n";
+            msg += $"Other Sales: {this.Other_Sales}\n";
+            msg += $"Global Sales: {this.Global_Sales}\n";
+            msg += "---------------------------------------";
+            return msg;
+        }
+    }
+}
